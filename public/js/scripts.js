59fb720655baa122fedc63768fcb5c8747bf02aa
hreis1 <ph.reis@discente.ufma.br>
2023-11-21 00:43:00 -0300

    Adicionando funções de calculadora

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,20 @@
 
 function insert(num){
-    
+    document.getElementById('resultado').innerHTML += num;
 }
 function clean(){
-    
+    document.getElementById('resultado').innerHTML = "";
 }
 function back(){
-    
+    document.getElementById('resultado').innerHTML = document.getElementById('resultado').innerHTML.slice(0, -1);
 }
 function calcular(){
-    
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }else{
+        document.getElementById('resultado').innerHTML = "Nada para calcular";
+    }
 }
 
 module.exports = {insert, clean, back, calcular}
